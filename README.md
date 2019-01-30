bf655b56de50409c2f12f43ad19e7e62ad42c27b
Bara Limbong <baralimbong@gmail.com>
2019-01-30 10:20:40 +0700

    javascriptnumber

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # learn-javascript-number
 
-learn javascript number
-weekend task
+## learn javascript number
--- index.js
+++ index.js
@@ -3,13 +3,28 @@ alert('hello')
 const number1 = 5
 const number2 = 10
 const number3 = 15
-console.log('number1 = ${number1}')
-console.log('number2 = ${number2}')
-console.log('number3 = ${number2}')
+console.log(`number1 = ${number1}`)
+console.log(`number2 = ${number2}`)
+console.log(`number3 = ${number2}`)
 
 const result1 = number1 + number2
 const result2 = number2 + number3
-const result3 = number2 + number3
-console.log(result1)
-console.log(result2)
-console.log(result3)
+const result3 = number1 + number3
+
+console.log(`result1 = ${number1} + ${number2} = ${result1}`)
+console.log(`result2 = ${number2} + ${number3} = ${result2}`)
+console.log(`result3 = ${number2} + ${number3} = ${result2}`)
+
+const string1 = 'F1ZR'
+const string2 = 'SATRIA R'
+const string3 = 'RX-KING'
+
+console.log(`${string1} ${string2} ${string3}`)
+
+const stringnumber1 = string1 + number1
+const stringnumber2 = string2 + number2
+const stringnumber3 = string3 + number3
+
+console.log(`stringnumber1 ${string1} + ${number1}`)
+console.log(`stringnumber2 ${string2} + ${number2}`)
+console.log(`stringnumber3 ${string3} + ${number3}`)
